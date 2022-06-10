f45496fad9fbbabdefd55c8e70b3c6719cc9de77
DaeWon <qkreodnjs97@naver.com>
2022-06-10 22:49:56 +0900

    [ADD] router추가

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id = product-explain>
+    This page is ProductPage
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+#product-explain {
+  margin-top: 100px;
+  width: 100%;
+  height: 500px;
+  background-color: #ecfbcc;
+}
+</style>
