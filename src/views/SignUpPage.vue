171301683efda5076c8ac213946b983d29b111a8
DaeWon <qkreodnjs97@naver.com>
2022-06-12 01:47:04 +0900

    [ADD] 생년월일 선택에서 선택한 달마다 dayList가 변경되도록 추가

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -17,37 +17,24 @@
                     <input type="text" name="id" value="" class="form-control input-lg" placeholder="아이디"/>
                     <input type="password" name="password" value="" class="form-control input-lg" placeholder="비밀번호"/>
                     <input type="password" name="confirm_password" value="" class="form-control input-lg" placeholder="비밀번호 확인"/>
-                    <input type="text" name="email" value="" class="form-control input-lg" placeholder="이메일"/>
-                    <input type="text" name="address" value="" class="form-control input-lg" placeholder="주소"/>
+                    <input type="email" name="email" value="" class="form-control input-lg" placeholder="이메일"/>
+                    <input type="address" name="address" value="" class="form-control input-lg" placeholder="주소"/>
                     <label>생년월일</label>
                     <div class="row">
                         <div class="col-xs-4 col-md-4">
-                            <select name="month" class = "form-control input-lg">
-                                <option value="01">Jan</option><option value="02">Feb</option><option value="03">Mar</option>
-                                <option value="04">Apr</option><option value="05">May</option><option value="06">Jun</option>
-                                <option value="07">Jul</option><option value="08">Aug</option><option value="09">Sep</option>
-                                <option value="10">Oct</option><option value="11">Nov</option><option value="12">Dec</option>
+                            <select name="year" class = "form-control input-lg">
+                                <option v-for="yearValue in 100" :key="2022 - yearValue" value=key>{{ 2022 - yearValue }}년</option>
+                            </select>
+                        </div>
+                        <div class="col-xs-4 col-md-4">
+                            <select id = "month" name="month" class = "form-control input-lg" @change="setMaxDay">
+                                <option v-for="monthValue in 12" :key="monthValue" value=monthValue>{{ monthValue }}월</option>
                             </select>
                         </div>
                         <div class="col-xs-4 col-md-4">
                             <select name="day" class = "form-control input-lg">
-                                <option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option>
-                                <option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option>
-                                <option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option>
-                                <option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option>
-                                <option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="21">21</option>
-                                <option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option>
-                                <option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option>
-                                <option value="30">30</option><option value="31">31</option>
-                            </select>                        
-                        </div>
-                        <div class="col-xs-4 col-md-4">
-                            <select name="year" class = "form-control input-lg">
-                                <option value="2001">2001</option><option value="2002">2002</option><option value="2003">2003</option><option value="2004">2004</option>
-                                <option value="2005">2005</option><option value="2006">2006</option><option value="2007">2007</option><option value="2008">2008</option>
-                                <option value="2009">2009</option><option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option>
-                                <option value="2013">2013</option>
-                            </select>
+                                <option v-for="dayValue in maxDay" :key="dayValue" value=dayValue>{{ dayValue }}일</option>
+                            </select>                    
                         </div>
                     </div>
                     <div id="gender-pick">
@@ -59,7 +46,7 @@
                             <input type="radio" name="gender" value="F" id=female />여
                         </label>
                     </div>
-                    <br /><br /><br />
+                    <br/><br/><br/>
                     <button class="btn btn-lg btn-primary btn-block signup-btn" type="submit">회원가입</button>
                 </form>          
             </div>
@@ -69,7 +56,33 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            maxDay: 31
+        }
+    },
+    methods: {
+    setMaxDay() {
+        let box = document.getElementById('month');
+        let selectedMonth = box.options[box.selectedIndex].text;
+        switch (selectedMonth)
+        {
+            case "2월":
+                this.maxDay = 28;
+                break;  
+            case "4월":
+            case "6월":
+            case "8월":
+            case "9월":
+            case "11월":
+                this.maxDay = 30;
+                break;
+            default:
+                this.maxDay = 31;
+                break;
+        }
+    }
+  }
 }
 </script>
 
