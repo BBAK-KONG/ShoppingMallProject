e020007999a903a8f6d9f1ebeb57bc63a026e88b
ming-taro <hera002008@naver.com>
2022-06-11 18:06:29 +0900

    [ADD] 메인페이지에 베스트 상품 코너 추가

---
+++ src/components/BestSeller.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>주간 베스트 상품</h1>
+  <div class="container">
+    <div class="row g-5">
+      <div class="col-4">
+        <div class="card">
+          <img src="@/assets/best-seller/Electronic-Clock-Ryan&Choonsik.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">라이언과 춘식이의 전자시계</h5>
+            <p class="card-text">49,000원</p>
+            <a href="#" class="btn btn-primary">구매하기</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card">
+          <img src="@/assets/best-seller/Face-Type-Mini-Cushion-Ryan.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">라이언 리틀 얼굴쿠션</h5>
+            <p class="card-text">16,000원</p>
+            <a href="#" class="btn btn-primary">구매하기</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card">
+          <img src="@/assets/best-seller/Ice-Mug-Apeach.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">시원한아이스머그_어피치</h5>
+            <p class="card-text">10,000원</p>
+            <a href="#" class="btn btn-primary">구매하기</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="best-seller-button">
+      <button type="button" class="btn btn-outline-success">베스트 상품 더 보기</button>
+    </div>
+
+    
+  </div>
+</template>
+
+<style>
+
+  .card :hover{
+    transform: scale(1.1);
+  }
+
+  .best-seller-button{
+    margin: 30px 0px 30px 0px;
+  }
+
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,14 +4,21 @@
         <BannerPart/>
       </div>
   </div>
+  <div id="best-seller">
+      <div class="best-seller-inner">
+        <BestSeller/>
+      </div>
+  </div>
 </template>
 
 <script>
 import BannerPart from '../components/BannerPart.vue';
+import BestSeller from '../components/BestSeller.vue';
 
 export default {
   components: {
-    BannerPart
+    BannerPart,
+    BestSeller
   }
 }
 </script>
@@ -20,13 +27,25 @@ export default {
 #banner {
   margin-top: 100px;
   width: 100%;
-  height: 500px;
+  height: 600px;
   background-color: #ecfbcc;
 } 
 
 .banner-inner {
   width: 1200px;
-  height: 500px;
+  height: 600px;
+  margin: 0 auto;
+}
+
+#best-seller {
+  margin-top: 100px;
+  width: 100%;
+  height: 600px;
+} 
+
+.best-seller-inner {
+  width: 1200px;
+  height: 600px;
   margin: 0 auto;
 }
 </style>
