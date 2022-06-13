6251c1eca5a3128ddaf9d6f4deb9c8bb4108f824
DaeWon <qkreodnjs97@naver.com>
2022-06-14 01:24:39 +0900

    [ADD] 카카오 주소 API 추가

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -19,7 +19,19 @@
                     <input type="password" name="password" value="" class="form-control input-lg" placeholder="비밀번호"/>
                     <input type="password" name="confirm_password" value="" class="form-control input-lg" placeholder="비밀번호 확인"/>
                     <input type="email" name="email" value="" class="form-control input-lg" placeholder="이메일"/>
-                    <input type="address" name="address" value="" class="form-control input-lg" placeholder="주소"/>
+
+                    <div class="row">
+                        <div class="col">
+                            <input type="text" id="postcode" name="postcode" value="" class="form-control input-lg" placeholder="우편번호">
+                        </div>
+                        <div class="col">
+                            <input type="button" @click="search" value="우편번호 찾기">
+                        </div>
+                    </div>
+
+                    <input type="text" id="address" class="form-control input-lg" placeholder="주소">
+                    <input type="text" id="detailAddress" class="form-control input-lg" placeholder="상세주소">
+
                     <label>생년월일</label>
                     <div class="row">
                         <div class="col-xs-4 col-md-4">
@@ -54,6 +66,7 @@
             </div>
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -82,8 +95,32 @@ export default {
                 this.maxDay = 31;
                 break;
         }
+    },
+    search() {
+        new window.daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var addr = ''; // 주소 변수
+
+                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                    addr = data.roadAddress;
+                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                    addr = data.jibunAddress;
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('postcode').value = data.zonecode;
+                document.getElementById("address").value = addr;
+                // 커서를 상세주소 필드로 이동한다.
+                document.getElementById("detailAddress").focus();
+            }
+        }).open();
     }
-  }
+    }
 }
 </script>
 
