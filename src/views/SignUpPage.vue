5a1692de51379c0120b61b545603e68fb70616c4
DaeWon <qkreodnjs97@naver.com>
2022-06-16 22:38:15 +0900

    [REFACTOR] document -> ref 사용으로 변경

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <img src="../assets/tattoo.png" class="rounded mx-auto d-block w-25" alt="상품이미지"> 
+        <img src="../assets/tattoo.png" class="rounded mx-auto d-block w-25" alt="상품이미지">
         <div class="row">
             <h3 class="col-sm-offset-3 col-sm-6">회원가입</h3>
         </div>
@@ -8,39 +8,45 @@
             <div class="col-sm-6">
                 <div class="row">
                     <div class="col-xs-6 col-md-6">
-                        <input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="성"/>
+                        <input type="text" name="firstname" ref="firstname" class="form-control input-lg" placeholder="성"/>
                     </div>
                     <div class="col-xs-6 col-md-6">
-                        <input type="text" name="lastname" id="lastname" class="form-control input-lg" placeholder="이름"/>
+                        <input type="text" name="lastname" ref="lastname" class="form-control input-lg" placeholder="이름"/>
                     </div>
+                    <span ref="nameCheck"> </span>
                 </div>
 
                 <div class="row">
                     <div class="col-9">
-                        <input type="text" name="id" id="id" class="form-control input-lg" placeholder="아이디 (영문자+숫자 조합 6~16자리)"/>
+                        <input type="text" name="id" ref="id" @change="checkRegexId" class="form-control input-lg" placeholder="아이디 (영문자+숫자 조합 6~16자리)"/>
                     </div>
                     <div class="col-3">
                         <button class="btn btn-block btn-primary" type="button" @click="idCheck">아이디 체크</button>
                     </div>
                 </div>
 
-                <input type="password" name="password" id="password" class="form-control input-lg" placeholder="비밀번호 (영문자+숫자+특수문자 조합 6~16자리)"/>
-                <input type="password" name="confirm_password" id="confirm_password" class="form-control input-lg" placeholder="비밀번호 확인"/>
-                <input type="tel" name="phone" id="phone" class="form-control input-lg" placeholder="핸드폰번호 (-없이 숫자만 입력)"/>
+                <span ref="idCheck"> </span>
+
+                <input type="password" name="password" ref="password" @change="checkRegexPw" class="form-control input-lg" placeholder="비밀번호 (영문자+숫자+특수문자 조합 6~16자리)"/>
+                <span ref="pwCheck"> </span>
+                <input type="password" name="confirm_password" ref="confirm_password" @change="checkConfirmPw" class="form-control input-lg" placeholder="비밀번호 확인"/>
+                <span ref="confirmPwCheck"> </span>
+                <input type="tel" name="phone" ref="phone" @change="checkPhone" class="form-control input-lg" placeholder="핸드폰번호 (-없이 숫자만 입력)"/>
+                <span ref="phoneCheck"> </span>
                 
                 
                 <div class="row">
                     <div class="col-4">
-                        <input type="text" name="email_id" id="email_id" class="form-control input-lg" placeholder="이메일"/>
+                        <input type="text" name="email_id" ref="email_id" class="form-control input-lg" placeholder="이메일"/>
                     </div>
                     <div class="col">
                         <label>@</label>
                     </div>
                     <div class="col-4">
-                        <input type="text" name="email_address" id="email_address" class="form-control input-lg"/>
+                        <input type="text" name="email_address" ref="email_address" class="form-control input-lg"/>
                     </div>
                     <div class="col-3">
-                        <select name="email_selected" id="email_selected" class="form-control input-lg" @change="changeEmailAddress">
+                        <select name="email_selected" ref="email_selected" class="form-control input-lg" @change="changeEmailAddress">
                             <option value="">직접입력</option> 
                             <option value="naver.com">naver.com</option>
                             <option value="gmail.com">gmail.com</option>
@@ -52,30 +58,30 @@
 
                 <div class="row">
                     <div class="col-9">
-                        <input type="text" id="postcode" name="postcode" value="" class="form-control input-lg" placeholder="우편번호" disabled>
+                        <input type="text" ref="postcode" id="postcode" name="postcode" value="" class="form-control input-lg" placeholder="우편번호" disabled>
                     </div>
                     <div class="col-3">
                         <button class="btn btn-block btn-primary" type="button" @click="searchAddress">우편번호 찾기</button>
                     </div>
                 </div>
 
-                <input type="text" id="address" class="form-control input-lg" placeholder="주소" disabled>
-                <input type="text" id="detailAddress" class="form-control input-lg" placeholder="상세주소">
+                <input type="text" ref="address" id="address" class="form-control input-lg" placeholder="주소" disabled>
+                <input type="text" ref="detailAddress" id="detailAddress" class="form-control input-lg" placeholder="상세주소">
 
                 <label>생년월일</label>
                 <div class="row">
                     <div class="col-xs-4 col-md-4">
-                        <select name="year" id="year" class = "form-control input-lg">
+                        <select name="year" ref="year" class = "form-control input-lg">
                             <option v-for="yearValue in 100" :key="2022 - yearValue">{{ 2022 - yearValue }}년</option>
                         </select>
                     </div>
                     <div class="col-xs-4 col-md-4">
-                        <select name="month" id="month" class = "form-control input-lg" @change="setMaxDay">
+                        <select name="month" ref="month" class = "form-control input-lg" @change="setMaxDay">
                             <option v-for="monthValue in 12" :key="monthValue">{{ monthValue }}월</option>
                         </select>
                     </div>
                     <div class="col-xs-4 col-md-4">
-                        <select name="day" id="day" class = "form-control input-lg">
+                        <select name="day" ref="day" class = "form-control input-lg">
                             <option v-for="dayValue in maxDay" :key="dayValue">{{ dayValue }}일</option>
                         </select>                    
                     </div>
@@ -83,10 +89,10 @@
                 <div id="gender-pick">
                     <label>성별 :</label>
                     <label class="radio-inline">
-                        <input type="radio" name="gender" value="M" id="male" />남
+                        <input type="radio" name="gender" value="M" ref="male" />남
                     </label>
                     <label class="radio-inline">
-                        <input type="radio" name="gender" value="F" id="female" />여
+                        <input type="radio" name="gender" value="F" ref="female" />여
                     </label>
                 </div>
                 <br/><br/><br/>
@@ -108,94 +114,98 @@ export default {
     },
     
     methods: {
-    checkValidation(){
+
+    checkRegexId(){
         let idRegexForm = /^[0-9a-zA-Z]{6,16}$/;
+        if(!idRegexForm.test(this.$refs.id.value) && this.$refs.id.value !== ""){
+            this.$refs.idCheck.innerHTML = "아이디를 영문자+숫자 조합으로 6~16자리로 입력해주세요";
+            this.$refs.id.focus();
+        }
+        else{
+            this.$refs.idCheck.innerHTML = " ";
+        }
+    },
+
+    checkRegexPw(){
         let passwordRegexForm = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*_+=-])(?=.*[0-9]).{6,16}$/;
-        let phoneRegexFrom = /^(01[0|1|6|7|8|9]|02|03[1-3]|04[1-4]|05[1-5]|06[1-4])([0-9]{3,4})([0-9]{4})$/;
-
-        let firstname = document.getElementById("firstname");
-        let lastname = document.getElementById("lastname");
-        let id = document.getElementById("id");
-        let password = document.getElementById("password");
-        let confirm_password = document.getElementById("confirm_password");
-        let phone = document.getElementById("phone");
-        let email_id = document.getElementById("email_id");
-        let email_address = document.getElementById("email_address");
-        let postcode = document.getElementById("postcode");
-        let address = document.getElementById("address");
-        let male = document.getElementById("male");
-        let female = document.getElementById("female");
-
-        if (firstname.value == ""){
-            alert("성을 입력해주세요");
-            firstname.focus();
-            return false;
+        if(!passwordRegexForm.test(this.$refs.password.value) && this.$refs.password.value !== ""){
+            this.$refs.pwCheck.innerHTML = "비밀번호를 영문자+숫자+특수문자 조합으로 6~16자리로 입력해주세요";
+            this.$refs.password.focus();
+        }
+        else{
+            this.$refs.pwCheck.innerHTML = " ";
         }
 
-        if (lastname.value == ""){
-            alert("이름을 입력해주세요");
-            lastname.focus();
-            return false;
-        }
+    },
 
-        if (id.value == ""){
-            alert("아이디를 입력해주세요");
-            id.focus();
-            return false;
-        }
-
-        if(!idRegexForm.test(id.value)){
-            alert("아이디를 영문자+숫자 조합으로 6~16자리로 입력해주세요");
-            id.focus();
-            return false;
-        }
-
-        if (password.value == ""){
-            alert("비밀번호를 입력해주세요");
-            password.focus();
-            return false;
-        }
-
-        if (!passwordRegexForm.test(password.value)){
-            alert("비밀번호를 영문자+숫자+특수문자 조합으로 6~16자리로 입력해주세요");
-            password.focus();
-            return false;
-        }
-
-        if (confirm_password.value !== password.value)
+    checkConfirmPw(){
+        if (this.$refs.confirm_password.value !== this.$refs.password.value)
         {
-            alert("비밀번호가 일치하지 않습니다");
-            confirm_password.focus();
+            this.$refs.confirmPwCheck.innerHTML = "비밀번호가 일치하지 않습니다";
+            this.$refs.confirm_password.focus();
+        }
+        else{
+            this.$refs.confirmPwCheck.innerHTML = " ";
+        }
+    },
+
+    checkPhone(){
+        let phoneRegexForm = /^(01[0|1|6|7|8|9]|02|03[1-3]|04[1-4]|05[1-5]|06[1-4])([0-9]{3,4})([0-9]{4})$/;
+        if(!phoneRegexForm.test(this.$refs.phone.value) && this.$refs.phone.value !== ""){
+            this.$refs.phoneCheck.innerHTML = "올바른 번호를 입력해주세요";
+            this.$refs.phone.focus();
+        }
+        else{
+            this.$refs.phoneCheck.innerHTML = " ";
+        }
+    },
+
+    checkValidation(){
+        if (this.$refs.firstname.value == ""){
+            alert("성을 입력해주세요");
+            this.$refs.firstname.focus();
             return false;
         }
 
-        if (!phoneRegexFrom.test(phone.value)){
-            alert("핸드폰번호를 형식에 맞게 입력해주세요");
-            phone.focus();
-            return false
+        if (this.$refs.lastname.value == ""){
+            alert("이름을 입력해주세요");
+            this.$refs.lastname.focus();
+            return false;
         }
 
-        if (email_id.value == ""){
+        if (this.$refs.id.value == ""){
+            alert("아이디를 입력해주세요");
+            this.$refs.id.focus();
+            return false;
+        }
+
+        if (this.$refs.password.value == ""){
+            alert("비밀번호를 입력해주세요");
+            this.$refs.password.focus();
+            return false;
+        }
+
+        if (this.$refs.email_id.value == ""){
             alert("이메일을 입력해주세요");
-            email_id.focus();
+            this.$refs.email_id.focus();
             return false;
         }
 
-        if (email_address.value == ""){
+        if (this.$refs.email_address.value == ""){
             alert("이메일 주소를 입력해주세요");
-            email_address.focus();
+            this.$refs.email_address.focus();
             return false;
         }
 
-        if (postcode.value == "" && address.value == ""){
+        if (this.$refs.postcode.value == "" && this.$refs.address.value == ""){
             alert("주소를 입력해주세요");
-            address.focus();
+            this.$refs.ddress.focus();
             return false;
         }
 
-        if (!male.checked && !female.checked){
+        if (!this.$refs.male.checked && !this.$refs.female.checked){
             alert("성별을 체크해주세요");
-            male.focus();
+            this.$refs.male.focus();
             return false;
         }
         this.sendData();
@@ -206,57 +216,50 @@ export default {
 
     idCheck(){
         let idRegexForm = /^[0-9a-zA-Z]{6,16}$/;
-        let id = document.getElementById("id");
-        let password = document.getElementById("password");
 
-        if (id.value == ""){
+        if (this.$refs.id.value == ""){
             alert("아이디 입력후에 체크해주세요");
         }
-        else if (idRegexForm.test(id.value)){
-            let id = document.getElementById("id");
-            
+        else if (idRegexForm.test(this.$refs.id.value)){            
             fetch("http://ec2-13-125-74-101.ap-northeast-2.compute.amazonaws.com:3000/users/exist", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                user_id: id.value,
+                user_id: this.$refs.id.value,
             }),
             })
             .then((response) => response.json())
             .then((data) => {
                 if (data['exist']){
                     alert("이미 사용중인 아이디 입니다.");
-                    id.value = "";
-                    id.focus();
+                    this.$refs.id.value = "";
+                    this.$refs.id.focus();
                 }
                 else{
                     alert("사용가능한 아이디 입니다.");
-                    password.focus();
+                    this.$refs.password.focus();
                 }
             }
             );
         }
         else{
             alert("아이디를 영문자+숫자 조합 6~16자리로 입력해주세요");
-            id.value = "";
-            id.focus();
+            this.$refs.id.value = "";
+            this.$refs.id.focus();
         }
     },
 
     changeEmailAddress(){
-        let email_address = document.getElementById("email_address");
-        let email_selected = document.getElementById("email_selected");
+        let selectedIndex = this.$refs.email_selected.options.selectedIndex;
+        let selectedEmailAddress = this.$refs.email_selected.options[selectedIndex].value;
 
-        let selectedIndex = email_selected.options.selectedIndex;
-        let selectedEmailAddress = email_selected.options[selectedIndex].value;
-
-        email_address.value = selectedEmailAddress;
+        this.$refs.email_address.value = selectedEmailAddress;
     },
 
     setMaxDay() {
-        let box = document.getElementById('month');
+        let box = this.$refs.month;
         let selectedMonth = box.options[box.selectedIndex].text;
         switch (selectedMonth)
         {
@@ -274,7 +277,7 @@ export default {
                 break;
         }
     },
-    
+
     searchAddress() {
         new window.daum.Postcode({
             oncomplete: function(data) {
@@ -297,27 +300,13 @@ export default {
     },
 
     sendData(){
-    let firstname = document.getElementById("firstname");
-    let lastname = document.getElementById("lastname");
-    let id = document.getElementById("id");
-    let password = document.getElementById("password");
-    let phone = document.getElementById("phone");
-    let email_id = document.getElementById("email_id");
-    let email_address = document.getElementById("email_address");
-    let address = document.getElementById("address");
-    let detailAddress = document.getElementById("detailAddress")
-    let year = document.getElementById("year")
-    let month = document.getElementById("month")
-    let day = document.getElementById("day")
-    let male = document.getElementById("male");
-    let female = document.getElementById("female");
     let gender;
 
-    if (male.checked){
-        gender = male.value;
+    if (this.$refs.male.checked){
+        gender = this.$refs.male.value;
     }
     else{
-        gender = female.value;
+        gender = this.$refs.female.value;
     }
     
     fetch("http://ec2-13-125-74-101.ap-northeast-2.compute.amazonaws.com:3000/users/register", {
@@ -326,14 +315,14 @@ export default {
         "Content-Type": "application/json",
     },
     body: JSON.stringify({
-        name: firstname.value + lastname.value,
-        user_id: id.value,
-        password: password.value,
-        phone: phone.value,
-        address: address.value + " " + detailAddress.value,
+        name: this.$refs.firstname.value + this.$refs.lastname.value,
+        user_id: this.$refs.id.value,
+        password: this.$refs.password.value,
+        phone: this.$refs.phone.value,
+        address: this.$refs.address.value + " " + this.$refs.detailAddress.value,
         gender: gender,
-        email: email_id.value + "@" + email_address.value,
-        birth: year.value + month.value + day.value
+        email: this.$refs.email_id.value + "@" + this.$refs.email_address.value,
+        birth: this.$refs.year.value + this.$refs.month.value + this.$refs.day.value
     }),
     })
 
@@ -345,22 +334,22 @@ export default {
 }
 </script>
 
+
 <style scoped>
 .container{
     background-color: #ecfbcc;
 }
-
 .col-sm-6{
     margin-left: 310px; 
-    padding-top: 20px;
 }
-
 .form-control { 
-    margin-bottom: 20px; 
+    margin-bottom: 10px; 
 }
-
 .radio-inline{
     padding-left: 10px;
+}
+span{
+    color : red
 }
 
 </style>
