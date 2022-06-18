d3c09f4eb65971604e30102416964af21953e68a
DaeWon <qkreodnjs97@naver.com>
2022-06-18 14:34:24 +0900

    [ADD] Session

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
-import { router } from './router'; 
+import { router } from './router'
+import { VueSession } from 'vue-session'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-
 import App from './App.vue'
 
 const app = createApp(App);
 
 app.use(router);
+
+var sessionOptions = {
+    persist: true
+  }
+app.use(VueSession, sessionOptions);
 
 app.mount('#app'); 
